0b7ee9bdde8578019c19cc0f53fad9b8fecda463
Noemi Ticona <noemi.ticona@bg.com.bo>
2024-01-09 20:32:27 -0400

    v5

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hostname=$(curl http://169.254.169.254/metadata/v1/hostname)
-docker run -d -p 80:80 --name propiedades-"$hostname" WebDanny:q/nurairbnb_propiedades:"$hostname"
+docker run -d -p 80:80 --name propiedades-"$hostname" dannyhs/propiedades:"$hostname"
